a560d9532c8ec29ca539ecfed8d83e0fef0ab51f
VrituuRuby <victor.velozo@outlook.com>
2022-08-19 23:52:09 -0300

    challenge completed - jest falhou em realizar alguns testes

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,11 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const emailAlreadySigned = this.usersRepository.findByEmail(email);
+
+    if (emailAlreadySigned) throw new Error("E-mail is already taken!");
+
+    return this.usersRepository.create({ name, email });
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,13 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+    try {
+      const userList = this.listAllUsersUseCase.execute({ user_id });
+      return response.status(200).json(userList);
+    } catch (Error) {
+      return response.status(400).json({ error: Error.message });
+    }
   }
 }
 
